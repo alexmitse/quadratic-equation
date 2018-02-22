a4b57738880a6d7bc95a4cc4eb9fc131023e139e
unknown <alex1998mitsevich@gmail.com>
2018-02-22 03:50:10 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let arr = equation.split(' ');
+    let D;
+    if (arr[7] == '-')
+        D = (+arr[4]) * (+arr[4]) + (4 * (+arr[0]) * ((+arr[8])));
+    else
+        D = (+arr[4]) * (+arr[4]) - (4 * (+arr[0]) * (+arr[8]));
+    let x1, x2;
+    if (arr[3] == '-') {
+        x1 = ((+arr[4]) + Math.sqrt(D)) / (2 * (+arr[0]));
+        x2 = ((+arr[4]) - Math.sqrt(D)) / (2 * (+arr[0]));
+    }
+    else {
+        x1 = (+(-arr[4]) + Math.sqrt(D)) / (2 * (+arr[0]));
+        x2 = (+(-arr[4]) - Math.sqrt(D)) / (2 * (+arr[0]));
+    }
+    x1 = Math.round(x1)
+    x2 = Math.round(x2);
+    let result = [];
+    if (x1 < x2)
+        result.push(x1, x2);
+    else
+        result.push(x2, x1);
+
+    return result;
 }
